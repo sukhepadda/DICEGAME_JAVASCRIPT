08baddb1a8e66415be11d942f823d2fabdc24cb4
sukhepadda <paddasukhwantsingh@gmail.com>
2023-01-05 19:02:40 +0530

    little change

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
 // console.log("testing")
 let num;
-const generate_number = () =>{
-    num = Math.floor(Math.random() * 6) + 1;
-    console.log(num);
-    
-}
+const generate_number = () => {
+  num = Math.floor(Math.random() * 6) + 1;
+  console.log(num);
+};
 
-const generate_num_btn_click = () =>{
-    document.querySelector('#sec-1').style.display = 'none';
-    document.querySelector('#sec-2').style.display = 'none';
-    document.querySelector('#sec-3').style.display = 'block';
+const generate_num_btn_click = () => {
+  document.querySelector("#sec-1").style.display = "none";
+  document.querySelector("#sec-2").style.display = "none";
+  document.querySelector("#sec-3").style.display = "block";
 
-    setTimeout(() => {
-        generate_number();
-        document.querySelector('#sec-1').style.display = 'none';
-        document.querySelector('#sec-2').style.display = 'block';
-        document.querySelector('#sec-3').style.display = 'none';
-        document.querySelector('#inp_field').value = "";
-        document.querySelector('#inp_field').focus();
-    }, 3000)
-}
-
+  setTimeout(() => {
+    generate_number();
+    document.querySelector("#sec-1").style.display = "none";
+    document.querySelector("#sec-2").style.display = "block";
+    document.querySelector("#sec-3").style.display = "none";
+    document.querySelector("#inp_field").value = "";
+    document.querySelector("#inp_field").focus();
+  }, 3000);
+};
 
 // check no
 
-const check_number = () =>{
-    let val = document.querySelector('#inp_field').value;
-    if(val == num){
-        alert("winner!");
-        generate_num_btn_click();
-    }else{
-        alert('oooooooooo!.....');
-        generate_num_btn_click();
+const check_number = () => {
+  let val = document.querySelector("#inp_field").value;
+  if (val == "") {
+    alert("pls enter value between 1-6");
+  } else {
+    if (val == num) {
+      alert("winner!");
+      generate_num_btn_click();
+    } else {
+      alert("oooooooooo!.....");
+      generate_num_btn_click();
     }
-}
+  }
+};
